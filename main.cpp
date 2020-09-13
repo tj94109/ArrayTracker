cabfb91704c99c0151ef6a80f16b6ae7b7b31d8d
TJ <tsteve11@mail.ccsf.edu>
2020-09-13 16:36:38 -0700

    first print of an array

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    const int ARRAY_SIZE = 7;
+    int anArray[] ={4, 5, 2, 1, 5, 6, 7};
+    for(int i=0; i<ARRAY_SIZE; i++){
+        if(i!= 0){
+            cout << " , ";
+        }
+        cout << anArray[i];
+    }
+    cout << "\n";
 }
