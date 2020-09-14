6f93891f613d0ce7dba5ff0c297f1f24df87adfb
TJ <tsteve11@mail.ccsf.edu>
2020-09-13 17:03:46 -0700

    Finished this first demo#

--- main.cpp
+++ main.cpp
@@ -14,10 +14,26 @@ void displayArray(int anArray[]){
     cout << "\n";
 }
 
+void badSort(int anArray[]){
+    bool haveDoneSwap = true;
+    while(haveDoneSwap){
+        haveDoneSwap = false;
+        for(int i = 0; i < ARRAY_SIZE -1; i++){
+            if(anArray[i] > anArray[i+1]){
+                haveDoneSwap = true;
+                int temp = anArray[i];
+                anArray[i] = anArray[i+1];
+                anArray[i+1] = temp;
+            }
+       }
+    }
+}
+
 int main()
 {
     int anArray[] ={4, 5, 2, 1, 5, 6, 7};
     displayArray(anArray);
-
+    badSort(anArray);
+    displayArray(anArray);
 
 }
