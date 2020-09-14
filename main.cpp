5da3b6bb129ab80271cfedade48ad3790839cd16
TJ <tsteve11@mail.ccsf.edu>
2020-09-13 17:19:21 -0700

    added arrayInteractions

--- main.cpp
+++ main.cpp
@@ -14,26 +14,35 @@ void displayArray(int anArray[]){
     cout << "\n";
 }
 
-void badSort(int anArray[]){
+int badSort(int anArray[]){
     bool haveDoneSwap = true;
+    int arrayInteractions = 0;
+    int temp = 0;
     while(haveDoneSwap){
         haveDoneSwap = false;
         for(int i = 0; i < ARRAY_SIZE -1; i++){
+            arrayInteractions +=2;
             if(anArray[i] > anArray[i+1]){
                 haveDoneSwap = true;
-                int temp = anArray[i];
+                arrayInteractions+=1;
+                temp = anArray[i];
+                arrayInteractions +=2;
                 anArray[i] = anArray[i+1];
+                arrayInteractions +=1;
                 anArray[i+1] = temp;
             }
        }
+
     }
+    return arrayInteractions;
 }
 
 int main()
 {
     int anArray[] ={4, 5, 2, 1, 5, 6, 7};
     displayArray(anArray);
-    badSort(anArray);
+    int arrayInteractions = badSort(anArray);
+    cout << "data actions: " << arrayInteractions << "\n";
     displayArray(anArray);
 
 }
